dd4e5ccc2afb37c09a8f0fb63be2aaf9c1bf95d2
Vladimir Muravev <vladimircode@icloud.com>
2023-02-23 09:53:38 +0300

    Initial Commit

---
+++ StarbucksClone/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  StarbucksClone
+//
+//  Created by Владимир Муравьев on 23.02.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
---
+++ StarbucksClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarbucksClone
+//
+//  Created by Владимир Муравьев on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
