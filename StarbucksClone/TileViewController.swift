d4d6de80a017b112bc36f25ea61e2c8f3861058f
Vladimir Muravev <vladimircode@icloud.com>
2023-02-26 08:54:04 +0300

    create and setup Tiles VCs

---
+++ StarbucksClone/TileViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TileViewController.swift
+//  StarbucksClone
+//
+//  Created by Владимир Муравьев on 25.02.2023.
+//
+
+import UIKit
+
+class TileViewController: UIViewController {
+    
+    let tileView = TileView()
+    
+    init(title: String, subtitle: String, imageName: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        tileView.titleLabel.text = title
+        tileView.subtitleLabel.text = subtitle
+        tileView.imageView.image = UIImage(named: imageName)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+    
+    func style() {
+        tileView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout() {
+        view.addSubview(tileView)
+        
+        NSLayoutConstraint.activate([
+            tileView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tileView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tileView.topAnchor.constraint(equalTo: view.topAnchor),
+            tileView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+   
+
+}
